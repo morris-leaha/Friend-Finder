3e7867a372b4b7fa28cf560ed41b42f9f21233f7
LAMorris <morris.leaha@gmail.com>
2019-03-07 15:45:51 -0600

    initial server.js setup | added 'watch' for nodemon

---
+++ server.js
@@ -0,0 +1,23 @@
+// ============ DEPENDENCIES ============
+
+var express = require("express");
+
+// ============ EXPRESS CONFIG ===========
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ============= ROUTER INFO ==============
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// ============= LISTENER INFO ==============
+
+app.listen(PORT, function() {
+    console.log("FriendFinder is listening on PORT: " + PORT);
+});
