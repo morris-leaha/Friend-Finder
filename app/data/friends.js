8164d6addc32546abb352fef0c5504caca46692e
LAMorris <morris.leaha@gmail.com>
2019-03-07 16:52:48 -0600

    initial friends.js setup | initial html setup

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+// (Adapted from 13.3-HotRestaurant in-class activity)
+// ============ DATA ============
+
+// Declare an empty array to 'store' all information
+
+var friendArray = [];
+
+// Export the array - make it accessible to other files using require
+
+module.exports = friendArray;
