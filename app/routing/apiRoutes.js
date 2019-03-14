894c03499617ff626a3f6f377d00c0158069ad3f
LAMorris <morris.leaha@gmail.com>
2019-03-14 13:28:55 -0500

    finished comparison logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,6 +16,33 @@ module.exports = function(app) {
     // NOTE: this is also where logic for compatibility will go!
     app.post("/api/friends", function(req, res) {
 
+        // store current user object in variable
+        var currentUser = req.body;
+        var bestMatch = {};
+        var bestScore = 100;
+
+
+        // loop over all users
+        for (var i=0; i < surveyData.length; i++) {
+            // loop over current user scores to compare with other user scores
+            var totalDiff = 0;
+
+            for (var j=0; j < currentUser.scores.length; j++) {
+                totalDiff += Math.abs(currentUser.scores[j] - surveyData[i].scores[j]);
+            }
+
+            if (totalDiff < bestScore) {
+                bestMatch.name = surveyData[i].name;
+                bestMatch.photo = surveyData[i].photo;
+
+                bestScore = totalDiff;
+            }
+        }
+
+        surveyData.push(currentUser);
+        res.json(bestMatch);
+
+
     });
 
 };
