f36ffba1f6f389aa055be645d4f122d336aae09f
LAMorris <morris.leaha@gmail.com>
2019-03-14 13:36:12 -0500

    trying to get css working

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+
+app.use(express.static(__dirname + "public"));
 
 // ============= ROUTER INFO ==============
 
