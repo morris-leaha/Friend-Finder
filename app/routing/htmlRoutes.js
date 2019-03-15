5d2f185df71ac9d22b33ecea75a743a29eadf600
LAMorris <morris.leaha@gmail.com>
2019-03-15 01:34:32 -0500

    added get route for home.html in htmlRoutes.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,6 +7,10 @@ var path = require("path");
 
 module.exports = function(app) {
 
+    app.get("/", function(req,res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+
     // a "GET" route to display the survey page
     app.get("/survey", function(req,res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
